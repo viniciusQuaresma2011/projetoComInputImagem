cc02313bd8d27071e4398e588a28d62961f4c8e0
unknown <viniciusquaresma14@hotmail.com>
2022-01-14 11:22:13 -0300

    projeto_upload_imagem

---
+++ src/main/java/com/halim/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.halim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//the main class
+public class SpringBootWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
